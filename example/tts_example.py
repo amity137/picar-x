237a0f9f21a0a0db4b0d8d5cc0e27cee0e3bea97
Amit Yadav <amity@microsoft.com>
2022-02-09 17:03:03 -0800

    speaker loud setting

--- example/tts_example.py
+++ example/tts_example.py
@@ -1,11 +1,16 @@
 import sys
 sys.path.append(r'/home/pi/picar-x/lib')
 from tts import TTS
+import time
 
 
 if __name__ == "__main__":
     words = ["Hello", "Hi", "Good bye", "Nice to meet you"]
     tts_robot = TTS()
+    #tts_robot.lang("de-DE")
+    tts_robot.espeak_params(amp=199)
+
     for i in words:
         print(i)
         tts_robot.say(i)
+        time.sleep(1)
